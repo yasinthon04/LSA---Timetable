ef19a5f3536a56d7929bb306b90f8edc16eaa1ec
Yasinthon Roekkamyee <yasinthonr@gmail.com>
2026-02-15 21:31:45 +0700

    Check if DATABASE_URL exists on Vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,6 +4,12 @@ const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
+if (!process.env.DATABASE_URL) {
+    console.error("❌ DATABASE_URL is not defined in environment variables!");
+} else {
+    console.log("🌏 DATABASE_URL is detected.");
+}
+
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
